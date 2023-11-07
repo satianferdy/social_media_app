94fcfdcd37a0bee672c512a8f90669d676cdf47b
satianferdy <satianferdy@gmail.com>
2023-11-07 22:11:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/pages/login_page.dart';
+import 'package:social_media_app/theme/dark_mode.dart';
+import 'package:social_media_app/theme/light_mode.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const loginPage(),
+      theme: lightMode,
+      darkTheme: darkMode,
+    );
+  }
+}
