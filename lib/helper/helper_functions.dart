a27158cc43a528fd7153b5834b12e3b95db38aa2
satianferdy <satianferdy@gmail.com>
2023-11-22 17:33:12 +0700

    fix: display error message :bug:

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -4,6 +4,20 @@ import 'package:flutter/material.dart';
 void displayErrorMessage(BuildContext context, String message) {
   showDialog(
     context: context,
-    builder: (context) => AlertDialog(),
+    builder: (context) => AlertDialog(
+      title: const Text("an error occured"),
+      content: Text(message),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            "OK",
+            style: TextStyle(color: Colors.grey[800]),
+          ),
+        ),
+      ],
+    ),
   );
 }
