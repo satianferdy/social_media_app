d6244cc22ea1dfc00ecb45ac89a142a4e6474d1d
satianferdy <satianferdy@gmail.com>
2023-11-23 21:31:10 +0700

    feat: get user data & edit profile and users page :rocket:

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,20 +1,103 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_app/components/my_back_button.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  // current logged in user
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  // future fetch user details
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Profile'),
-        ),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Text('Profile Page'),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: getUserDetails(),
+        builder: (context, snapshot) {
+          // loading indicator
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          // error
+          else if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          }
+
+          // data received
+          else if (snapshot.hasData) {
+            // user details
+            Map<String, dynamic>? userDetails = snapshot.data!.data();
+
+            return Center(
+              child: Column(
+                children: [
+                  // back button
+                  const Padding(
+                    padding: EdgeInsets.only(top: 50, left: 25),
+                    child: Row(
+                      children: [
+                        MyBackButton(),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // user profile icon
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    padding: const EdgeInsets.all(25),
+                    child: const Icon(
+                      Icons.person,
+                      size: 80,
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+                  // user email
+                  Text(
+                    userDetails!['username'],
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+                  // user username
+                  Text(
+                    userDetails!['email'],
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return const Center(
+              child: Text("Error: Something went wrong maybe no"),
+            );
+          }
+        },
       ),
     );
   }
--- lib/pages/users.page.dart
+++ lib/pages/users.page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_app/components/my_back_button.dart';
+import 'package:social_media_app/helper/helper_functions.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({super.key});
@@ -6,15 +9,59 @@ class UsersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Users'),
-        ),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Text('Users Page'),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('users').snapshots(),
+        builder: (context, snapshot) {
+          // any errors
+          if (snapshot.hasError) {
+            displayErrorMessage(context, "Something went wrong");
+          }
+          // show loading indicator
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data == null) {
+            return const Center(
+              child: Text("No users found"),
+            );
+          }
+
+          // get all users
+          final users = snapshot.data!.docs;
+
+          return Column(
+            children: [
+              // back button
+              const Padding(
+                padding: EdgeInsets.only(top: 50, left: 25),
+                child: Row(
+                  children: [
+                    MyBackButton(),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: users.length,
+                  padding: EdgeInsets.zero,
+                  itemBuilder: (context, index) {
+                    // get individual user details
+                    final userDetails = users[index];
+
+                    return ListTile(
+                      title: Text(userDetails['username']),
+                      subtitle: Text(userDetails['email']),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
